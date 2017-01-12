8b04eb598678e1924561f00bbe850118a78d7881
Mitch Allen <mitch.c.allen@gmail.com>
2017-01-12 15:13:57 -0700

    updated test files

---
+++ test/dist/dist-smoke-test.js
@@ -0,0 +1,43 @@
+/**
+    Module: @mitchallen/aframe-border-component
+      Test: smoke-test-dist
+    Author: Mitch Allen
+*/
+
+"use strict";
+
+var request = require('supertest'),
+    should = require('should'),
+    modulePath = "../../dist/aframe-border-component";
+
+describe('dist smoke test', () => {
+
+    var script = null;
+
+    before( done => {
+        // Call before all tests
+        delete require.cache[require.resolve(modulePath)];
+        script = require(modulePath);
+        done();
+    });
+
+    after( done => {
+        // Call after all tests
+        done();
+    });
+
+    beforeEach( done => {
+        // Call before each test
+        done();
+    });
+
+    afterEach( done => {
+        // Call after eeach test
+        done();
+    });
+
+    it('script should exist', done => {
+        should.exist(script);
+        done();
+    })
+});
